cd0c4992048a5361e26b600c30f0c22d437da3c3
MagdalenaBrejna <magdalena.brejna@wp.pl>
2021-11-15 17:57:52 +0100

    feat: Add initial maven's dependencies

---
+++ src/main/java/mb/seeme/SeeMeApplication.java
@@ -0,0 +1,13 @@
+package mb.seeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mb/seeme/SeeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.seeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
