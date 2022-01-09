de7298c4d73d8bea17550a8c1dfabf40deb6a293
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-01-09 17:43:04 +0100

    feat: Add a modal with term details in the provider archive
    Create a "TermController" and a method "deleteTerm" that deletes a term
    with a given id and makes a redirect to the provider archive. Add a modal
    in the "archive.html" with term details and a modal open button for every
    table row. Update the "TermBootstrap" by adding terms' descriptions.

--- src/main/java/mb/seeme/bootstrap/TermBootstrap.java
+++ src/main/java/mb/seeme/bootstrap/TermBootstrap.java
@@ -50,8 +50,8 @@ public class TermBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
     @Transactional
     public List<Term> getTerms(){
-        Client clientA = Client.builder().id(1l).name("clientA").email("AC").password(passwordEncoder.encode("passAC")).userRole(CLIENT.getUserRole()).build();
-        Client clientB = Client.builder().id(2l).name("clientB").email("BC").password(passwordEncoder.encode("passBC")).userRole(CLIENT.getUserRole()).build();
+        Client clientA = Client.builder().id(1l).name("clientA").email("AC").password(passwordEncoder.encode("passAC")).userRole(CLIENT.getUserRole()).telephone("111111111").build();
+        Client clientB = Client.builder().id(2l).name("clientB").email("BC").password(passwordEncoder.encode("passBC")).userRole(CLIENT.getUserRole()).telephone("222222222").build();
         clientRepository.save(clientA);
         clientRepository.save(clientB);
 
@@ -65,14 +65,14 @@ public class TermBootstrap implements ApplicationListener<ContextRefreshedEvent>
         availableServiceRepository.save(service1);
         availableServiceRepository.save(service2);
 
-        Term term1 = Term.builder().id(1l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("12:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientB).termRealizedStatus(Status.FULL).build();
-        Term term2 = Term.builder().id(2l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("13:32:22", DateTimeFormatter.ISO_TIME)).service(service2).client(clientA).termRealizedStatus(Status.FULL).build();
-        Term term3 = Term.builder().id(3l).termDate(LocalDate.parse("2021-11-20")).termTime(LocalTime.parse("14:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientA).termRealizedStatus(Status.FULL).build();
-        Term term4 = Term.builder().id(4l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientA).termRealizedStatus(Status.FULL).build();
-        Term term5 = Term.builder().id(5l).termDate(LocalDate.parse("2022-11-22")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service2).termRealizedStatus(Status.FREE).build();
-        Term term6 = Term.builder().id(6l).termDate(LocalDate.parse("2022-11-15")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FREE).build();
-        Term term7 = Term.builder().id(7l).termDate(LocalDate.parse("2022-11-23")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FREE).build();
-        Term term8 = Term.builder().id(8l).termDate(LocalDate.parse("2022-11-14")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FULL).client(clientA).build();
+        Term term1 = Term.builder().id(1l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("12:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientB).termRealizedStatus(Status.FULL).termDescription("Naprawa Skoda Superb").build();
+        Term term2 = Term.builder().id(2l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("13:32:22", DateTimeFormatter.ISO_TIME)).service(service2).client(clientA).termRealizedStatus(Status.FULL).termDescription("Strzyzenie meskie").build();
+        Term term3 = Term.builder().id(3l).termDate(LocalDate.parse("2021-11-20")).termTime(LocalTime.parse("14:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientA).termRealizedStatus(Status.FULL).termDescription("Wymiana opon").build();
+        Term term4 = Term.builder().id(4l).termDate(LocalDate.parse("2021-11-19")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).client(clientA).termRealizedStatus(Status.FULL).termDescription("Uzupelnienie filtrow").build();
+        Term term5 = Term.builder().id(5l).termDate(LocalDate.parse("2022-11-22")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service2).termRealizedStatus(Status.FREE).termDescription("Farbowanie wlosow").build();
+        Term term6 = Term.builder().id(6l).termDate(LocalDate.parse("2022-11-15")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FREE).termDescription("Montaz kamery tylniej").build();
+        Term term7 = Term.builder().id(7l).termDate(LocalDate.parse("2022-11-23")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FREE).termDescription("Naprawa szkody Fiat 500 z ubezpieczenia").build();
+        Term term8 = Term.builder().id(8l).termDate(LocalDate.parse("2022-11-14")).termTime(LocalTime.parse("09:32:22", DateTimeFormatter.ISO_TIME)).service(service1).termRealizedStatus(Status.FULL).client(clientA).termDescription("Lakierowanie maski").build();
 
         List<Term> termList = new ArrayList<Term>();
         termList.add(term1);
---
+++ src/main/java/mb/seeme/controllers/TermController.java
@@ -0,0 +1,26 @@
+package mb.seeme.controllers;
+
+import mb.seeme.services.terms.TermService;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.annotation.security.RolesAllowed;
+
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Controller
+public class TermController {
+
+    private final TermService termService;
+
+    public TermController(TermService termService) {
+        this.termService = termService;
+    }
+
+    @RolesAllowed("PROVIDER")
+    @GetMapping({"delete/{id}", "delete/{id}.html"})
+    public String deleteTerm(@PathVariable("id") Long termId){
+        termService.deleteById(termId);
+        return "redirect:/providers/archive";
+    }
+}
