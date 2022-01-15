d660a48ed041d5094eddc2a4310701a18ac53ecb
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-01-15 19:54:50 +0100

    bug: Remove delete button in the term's details modal in the provider's archive

--- src/main/java/mb/seeme/controllers/TermController.java
+++ src/main/java/mb/seeme/controllers/TermController.java
@@ -1,11 +1,11 @@
 package mb.seeme.controllers;
 
 import mb.seeme.services.terms.TermService;
-import mb.seeme.services.users.UserAuthenticationService;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import javax.annotation.security.RolesAllowed;
 
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
@@ -13,18 +13,9 @@ import javax.annotation.security.RolesAllowed;
 public class TermController {
 
     private final TermService termService;
-    private final UserAuthenticationService userAuthenticationService;
 
-    public TermController(TermService termService, UserAuthenticationService userAuthenticationService) {
+    public TermController(TermService termService) {
         this.termService = termService;
-        this.userAuthenticationService = userAuthenticationService;
-    }
-
-    @RolesAllowed("PROVIDER")
-    @GetMapping({"delete/{id}", "delete/{id}.html"})
-    public String deleteTerm(@PathVariable("id") Long termId){
-        termService.deleteById(termId);
-        return "redirect:/providers/archive";
     }
 
     @RolesAllowed("CLIENT")
