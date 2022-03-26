48e35812d56a5dcd75db635e38cf62f033e2d6df
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-03-26 19:59:44 +0100

    refactor: Create a new main page layout
    Update the file "home.html", so that it uses carousel. Add necessary images.

--- src/main/java/mb/seeme/security/ApplicationAuthenticationSuccessHandler.java
+++ src/main/java/mb/seeme/security/ApplicationAuthenticationSuccessHandler.java
@@ -3,14 +3,13 @@ package mb.seeme.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class AppAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class ApplicationAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.sendRedirect("/");
     }
--- src/main/java/mb/seeme/security/ApplicationSecurityConfig.java
+++ src/main/java/mb/seeme/security/ApplicationSecurityConfig.java
@@ -3,31 +3,24 @@ package mb.seeme.security;
 import mb.seeme.jwt.JwtConfig;
 import mb.seeme.jwt.JwtTokenVerifier;
 import mb.seeme.jwt.JwtUsernameAndPasswordAuthenticationFilter;
-import mb.seeme.services.users.UserAuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
 import javax.crypto.SecretKey;
 import javax.servlet.http.Cookie;
 
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
-    private final UserAuthenticationService userAuthenticationService;
     private final SecretKey secretKey;
     private final JwtConfig jwtConfig;
 
-    public ApplicationSecurityConfig(UserAuthenticationService userAuthenticationService, PasswordEncoder passwordEncoder, SecretKey secretKey, JwtConfig jwtConfig) {
-        this.passwordEncoder = passwordEncoder;
-        this.userAuthenticationService = userAuthenticationService;
+    public ApplicationSecurityConfig(SecretKey secretKey, JwtConfig jwtConfig) {
         this.secretKey = secretKey;
         this.jwtConfig = jwtConfig;
     }
@@ -39,11 +32,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and().csrf().disable()
                     .addFilterBefore(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey), UsernamePasswordAuthenticationFilter.class)
                     .addFilterBefore(new JwtTokenVerifier(secretKey, jwtConfig), BasicAuthenticationFilter.class)
+
                     .authorizeRequests()
                     .antMatchers("/client/**").hasRole("CLIENT")
                     .antMatchers("/providers/**").hasRole("PROVIDER")
                     .antMatchers("/fragments/**").hasAnyRole("CLIENT","PROVIDER")
                     .antMatchers("/home").permitAll()
+
                     .and().formLogin().loginPage("/login").permitAll().successHandler(appAuthenticationSuccessHandler())
                     .and().httpBasic()
                     .and().logout(logout -> logout
@@ -63,6 +58,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public AuthenticationSuccessHandler appAuthenticationSuccessHandler(){
-        return new AppAuthenticationSuccessHandler();
+        return new ApplicationAuthenticationSuccessHandler();
     }
 }
